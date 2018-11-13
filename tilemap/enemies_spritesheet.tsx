3e508e962767a1e9694e78be72096d64adf58c85
Luciano Dias <302080087@student.rocmondriaan.nl>
2018-11-13 11:38:22 +0100

    map van level 1 toegevoegd

---
+++ tilemap/enemies_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="enemies_spritesheet" tilewidth="70" tileheight="70" tilecount="10" columns="5">
+ <image source="../../../platformerGraphicsDeluxeUpdated/Enemies/enemies_spritesheet.png" width="353" height="153"/>
+</tileset>
